8f77e5c83978ef1c800f0603f5330a47732576f4
lg0killer <schnetlerlouis@gmail.com>
2026-05-08 17:18:40 +0200

    Fixing scraping

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ echo "Running database migrations..."
 python manage.py migrate
 
 # Initialize schedules if they don't exist
-python manage.py shell -c "from django_celery_beat.models import PeriodicTask, IntervalSchedule; schedule_15, _ = IntervalSchedule.objects.get_or_create(every=15, period=IntervalSchedule.MINUTES); PeriodicTask.objects.get_or_create(name='Catchup Scrapers (Every 15 mins)', defaults={'interval': schedule_15, 'task': 'news.tasks.catchup_scrapers'})"
+python manage.py shell -c "from django_celery_beat.models import PeriodicTask, IntervalSchedule; schedule_15, _ = IntervalSchedule.objects.get_or_create(every=15, period=IntervalSchedule.MINUTES); task, created = PeriodicTask.objects.get_or_create(name='Catchup Scrapers (Every 15 mins)', defaults={'interval': schedule_15, 'task': 'apps.news.tasks.catchup_scrapers'}); task.task = 'apps.news.tasks.catchup_scrapers'; task.interval = schedule_15; task.save() if not created else None"
 
 exec "$@"
